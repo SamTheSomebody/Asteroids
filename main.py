8da3b0a7c34d7c8c06c0d48fe6b727af85070e3e
SamTheSomebody <gamedevsamdevelopment+github@proton.me>
2024-10-18 12:45:23 +1100

    Added Asteroids (Visual Only)

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black_color = "#000000FF"
-    dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
-    player = Player(x, y)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         for u in updatable:
             u.update(dt)
-        screen.fill(black_color)
+
+        screen.fill("black")
+
         for d in drawable:
             d.draw(screen)
+
         pygame.display.flip()
+
         interval = clock.tick(60)
         dt = interval / 1000
 
